f8fc27cb6d1cd06e786ae60e3ea83bf56e7f8fe6
unknown <se.bouidioua@esi-sba.dz>
2025-02-23 21:47:25 +0100

    something

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Login from './pages/Login.js'
 import ForgetPassword from './pages/ForgetPassword';
 
 function App() {
